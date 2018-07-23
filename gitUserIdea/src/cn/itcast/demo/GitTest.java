14781e8e4b2a6a3304785c3dd2c82a3b91d88f97
cy <1073321828@qq.com>
2018-07-23 16:54:30 +0800

    第一次提交代码

---
+++ gitUserIdea/src/cn/itcast/demo/GitTest.java
@@ -0,0 +1,9 @@
+package cn.itcast.demo;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("hello Git1");
+    }
+}
